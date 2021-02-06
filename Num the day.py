3dae469a4bddf459821dcf71f468325cabc387c1
RB-85 <r.g_badr@yahoo.com>
2021-02-06 17:33:55 +0300

    Create Num the day.py

---
+++ Num the day.py
@@ -0,0 +1,18 @@
+
+The_day=int(input('enter number the day, from 1 to 7 :'))
+if The_day == 1:
+  print('saturday')
+elif The_day == 2:
+  print('sunday')
+elif The_day == 3:
+  print('monday')
+elif The_day == 4:
+  print('tuesday')
+elif The_day == 5:
+  print('wednsday')
+elif The_day == 6:
+  print('thursday')
+elif The_day == 7:
+  print('friday')
+else:
+  print('invalid')
