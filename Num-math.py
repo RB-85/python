4d6a7f3c9851694e750744f8ad87be0eb8bae825
RB-85 <r.g_badr@yahoo.com>
2021-02-07 11:21:48 +0300

    Create Num-math.py

---
+++ Num-math.py
@@ -0,0 +1,13 @@
+
+num=int(input('please enter first number:'))
+num1=int(input('please enter second number:'))
+summation_result=num+num1
+subtraction_result=num-num1
+multiplication_result=num*num1
+division_result=num/num1
+remainder_result=num%num1
+print('the summation result is :' , summation_result)
+print('the subtraction result is :',subtraction_result)
+print('the multiplication result is :',multiplication_result)
+print('the division result is :',division_result)
+print('the remainder is :',remainder_result)
