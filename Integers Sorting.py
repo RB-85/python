34f372a7f48d01c01944c5fc39e11428e0ea6973
RB-85 <r.g_badr@yahoo.com>
2021-02-08 20:19:19 +0300

    Create Integers Sorting.py

---
+++ Integers Sorting.py
@@ -0,0 +1,23 @@
+
+x = int(input("Enter the first number: "))
+
+y = int(input("Enter the second number: "))
+
+z = int(input("Enter the third number: "))
+
+a1 = min(x,y,z)
+
+a2 = max(x,y,z)
+
+a3 =(x+y+z)-a1-a2
+
+print("The numbers in sorted order are: ")
+
+print(" ", a1)
+
+print(" ", a2)
+
+print(" ", a3)
+
+
+
